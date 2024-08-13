5274a1e11387242325b8b347689266f5cbdda7fa
Ikey Salgado <ikeysfr@gmail.com>
2024-08-13 15:01:25 -0300

    Aula de Operadores de Atribuicao

---
+++ Program.cs
@@ -0,0 +1,119 @@
+﻿string a = "15-";
+
+int b = 0;
+
+int.TryParse(a, out b);
+
+
+Console.WriteLine(b);
+Console.WriteLine("Conversão realizada com sucesso!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Ordem dos operadores
+
+// double a = 4 / (2 + 2);
+// Console.WriteLine(a);
+
+
+
+
+
+
+
+
+
+
+
+
+//Casting implicito
+
+// int a = 5;
+// double b = a;
+
+// int a = 5;
+// long b = a;
+
+// int a = int.MaxValue;
+// long b = a;
+
+// Console.WriteLine(b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int inteiro = 5;
+// string a = inteiro.ToString();
+
+// Console.WriteLine(a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //Cast - Casting
+// int a = Convert.ToInt32(null);
+// //int a = int.Parse(null);
+
+
+
+
+
+
+// int a = 10;
+// int b = 20;
+
+// int c = a + b;
+
+//c = c + 5; // 30 + 5
+//c += 5;
+
+//c = c-5
+//c -= 5;
+
+//c = c*5
+//c *= 5;
